fd31f900810e5d3a16b29aa6358db807105997ef
rimasjuzeliunass <rimas.juzeliunas@panko.lt>
2021-06-13 00:46:55 +0300

    as

--- book_store/settings.py
+++ book_store/settings.py
@@ -117,7 +117,7 @@ DJANGO_REDIS_CONNECTION_FACTORY = "core.redis.ConnectionFactory"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://:p74aa0bc9815d4f0441caef13fc776c3502fa54b6bf184ce3033738fc1915f7e4@ec2-34-254-104-245.eu-west-1.compute.amazonaws.com:11210",
+        "LOCATION": "redis://:p74aa0bc9815d4f0441caef13fc776c3502fa54b6bf184ce3033738fc1915f7e4@ec2-34-254-104-245.eu-west-1.compute.amazonaws.com:11210/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "REDIS_CLIENT_CLASS": "redis.client.StrictRedis",
